b5f05123a153a3d6b1e86c026ca8668403459dc8
Hady Salama <salama.hady@hotmail.com>
2022-02-16 23:43:11 -0500

    Part 2

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,10 +1,16 @@
 pragma solidity ^0.8.12;
 
-/* SPDX-License-Identifier: UNLICENSED */
+//SPDX-License-Identifier: UNLICENSED
 
 contract MyContract {
-    string value;
+    string public value = "Hello World!"; //Can also be string public constant.
+    bool public myBool = true;
+    int public myInt = 1;
+    uint public myUint = 1;
+    uint8 public myUint8 = 8;
+    uint256 public myUint256 = 123456789101112;
 
+/*
     constructor() {
         value = "Hello World!";
     }
@@ -12,7 +18,7 @@ contract MyContract {
     function get() public returns(string memory) {
         return value;
     }
-
+*/
     function set(string memory _value) public {
         value = _value;
     }
